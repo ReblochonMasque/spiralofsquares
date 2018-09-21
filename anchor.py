434b9a0a1eb286cc28ee2f55b7307b697321e4b5
ReblochonMasque <trustgold@gmail.com>
2018-09-21 20:04:02 +0800

    add anchor

---
+++ anchor.py
@@ -0,0 +1,28 @@
+"""
+The coordinates at which a rectangle is anchored on canvas
+
+"""
+
+
+class Anchor:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Anchor(self.x + other[0], self.y + other[1])
+
+    def __sub__(self, other):
+        return Anchor(self.x - other[0], self.y - other[1])
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+    def __getitem__(self, idx):
+        a = (self.x, self.y)
+        return a[idx]
+
+
+if __name__ == '__main__':
+    pass
